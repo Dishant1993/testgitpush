c03c7fc2a48f3902f15b1a1392cd86fda397bb30
Dishant1993 <dishant.salunke9@gmail.com>
2022-07-31 00:21:25 +0530

    this is my 1st code push

---
+++ mongotest.py
@@ -0,0 +1,13 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://Dishant19:Dishant19@cluster0.lwbln6v.mongodb.net/?retryWrites=true&w=majority")
+db=client.test
+print(db)
+
+d={
+    'name':'Dishant',
+    'surname':'Salunke'
+}
+
+db1= client['mongotest']
+coll =db1['test']
+coll.insert_one(d)
